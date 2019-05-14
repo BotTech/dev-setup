24f485ebe0625bf98f8b21c11ae5d44a830b9d58
Jason Pickens <jasonpickensnz@gmail.com>
2019-05-15 08:16:34 +1200

    Add automatic commit to the fork initialization

--- bin/init-fork.sh
+++ bin/init-fork.sh
@@ -6,4 +6,5 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 mv "${script_dir}/../TEMPLATE.md" "${script_dir}/../README.md"
 bash "${script_dir}/update-links.sh"
+git -C "${script_dir}/.." commit -am "Initialize fork"
 
