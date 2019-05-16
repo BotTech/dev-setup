1d4029af230dcb1445032b0a7ead5c03a65d7c8d
Jason Pickens <jasonpickensnz@gmail.com>
2019-05-16 19:33:18 +1200

    Fix update-links script

--- bin/update-links.sh
+++ bin/update-links.sh
@@ -3,25 +3,21 @@
 set -e
 
 readonly PREVIOUS_REMOTE_HTTP_URL="https://github.com/BotTech/dev-setup.git"
-readonly PREVIOUS_REMOTE_RAW_URL="https://raw.githubusercontent.com/BotTech/dev-setup.git"
-readonly PREVIOUS_REMOTE_GIT_URL="git@github.com:BotTech/dev-setup.git"
 readonly PREVIOUS_COMMIT=3497553571b3ff580478621cf278e21a1989a575
 
 escape_slashes() {
   echo "${1//\//\\/}"
 }
 
-# Escaped
-previous_remote_http_url="$(escape_slashes "${PREVIOUS_REMOTE_HTTP_URL}")"
-previous_remote_raw_url="$(escape_slashes "${PREVIOUS_REMOTE_RAW_URL}")"
-previous_remote_git_url="$(escape_slashes "${PREVIOUS_REMOTE_GIT_URL}")"
+previous_remote_url="${PREVIOUS_REMOTE_HTTP_URL}"
+previous_remote_http_url="${previous_remote_url%\.git}"
+previous_remote_http_url="$(escape_slashes "${previous_remote_http_url}")"
+previous_remote_raw_url="${previous_remote_http_url/github.com/raw.githubusercontent.com}"
+previous_remote_git_url="${previous_remote_http_url/https:\\\/\\\/github.com\\\//git@github.com:}"
 previous_commit="${PREVIOUS_COMMIT}"
 
-# Not escaped
-current_remote_http_url="$(git config --get remote.origin.url)"
-current_remote_url="${current_remote_http_url%\.git}"
-
-# Escaped
+current_remote_url="$(git config --get remote.origin.url)"
+current_remote_http_url="${current_remote_url%\.git}"
 current_remote_http_url="$(escape_slashes "${current_remote_http_url}")"
 current_remote_raw_url="${current_remote_http_url/github.com/raw.githubusercontent.com}"
 current_remote_git_url="${current_remote_http_url/https:\\\/\\\/github.com\\\//git@github.com:}"
