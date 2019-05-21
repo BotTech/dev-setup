19287aff8ff6656afcaafaf7fc16beaf9736a8b4
Jason Pickens <jasonpickensnz@gmail.com>
2019-05-21 19:31:01 +1200

    Add message to set iTerm2 Default profile

--- config/macos/module-config/iterm2/configure.sh
+++ config/macos/module-config/iterm2/configure.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-# Copy over the profiles.
 mkdir -p ~/"Library/Application Support/iTerm2/DynamicProfiles"
 cp profiles.json ~/"Library/Application Support/iTerm2/DynamicProfiles/"
+# TODO: Find a way to script this.
+#  https://gitlab.com/gnachman/iterm2/issues/7829
+echo 'Added a Dynamic Default profile to iTerm2.'
+echo 'You need to set this as the Default profile and/or delete the existing Default profile.'
 
 configure_shell_integration.sh
