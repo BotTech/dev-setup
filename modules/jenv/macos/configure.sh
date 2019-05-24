33dc81680fd04179f513fe4d955e0477cba11182
Jason Pickens <jasonpickensnz@gmail.com>
2019-05-25 10:20:22 +1200

    Automatically add installed JVMs to jenv

--- modules/jenv/macos/configure.sh
+++ modules/jenv/macos/configure.sh
@@ -5,5 +5,7 @@ $'export PATH="$HOME/.jenv/bin:$PATH"\n'\
 'eval "$(jenv init -)"'
 
 if_command_exists bash add_to_bash_profile "${jenv_init}"
+if_command_exists zsh echo 'Make sure that you add the jenv plugin to ZSH.'
 
-# TODO: If using oh-my-zsh then add plugin.
+[[ -d '/Library/Java/JavaVirtualMachines' ]] && \
+  find '/Library/Java/JavaVirtualMachines' -type f -path '*/Home/bin/*' -name 'java' -exec jenv add {} \;
