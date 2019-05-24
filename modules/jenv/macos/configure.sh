c783ffcf589a96d682713c8389599f37dd0d65d7
Jason Pickens <jasonpickensnz@gmail.com>
2019-05-25 10:34:21 +1200

    Fix jenv configuration

--- modules/jenv/macos/configure.sh
+++ modules/jenv/macos/configure.sh
@@ -8,4 +8,4 @@ if_command_exists bash add_to_bash_profile "${jenv_init}"
 if_command_exists zsh echo 'Make sure that you add the jenv plugin to ZSH.'
 
 [[ -d '/Library/Java/JavaVirtualMachines' ]] && \
-  find '/Library/Java/JavaVirtualMachines' -type f -path '*/Home/bin/*' -name 'java' -exec jenv add {} \;
+  find '/Library/Java/JavaVirtualMachines' -type d -path '*/Home' -exec jenv add {} \;
