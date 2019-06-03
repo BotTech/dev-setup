c31ef5d15b6d51b54093a3078bc5bdda0c323657
Jason Pickens <jasonpickensnz@gmail.com>
2019-06-04 09:37:10 +1200

    Add sbt-updates plugin configuration

---
+++ modules/sbt/macos/configure-sbt-updates-plugin.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+text='addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.4.0")'
+add_to_file ~/.sbt/1.0/plugins/sbt-updates.sbt "${text}"
+add_to_file ~/.sbt/1.0/plugins/plugins/sbt-updates.sbt "${text}"
+add_to_file ~/.sbt/0.13/plugins/sbt-updates.sbt "${text}"
+add_to_file ~/.sbt/0.13/plugins/plugins/sbt-updates.sbt "${text}"
