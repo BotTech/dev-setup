e13f23b12cd67585c21d10a1843a38e50ea64afe
Jason Pickens <jasonpickensnz@gmail.com>
2019-06-06 16:39:57 +1200

    Add TODO to oh-my-zsh module

--- modules/oh-my-zsh/macos/install.sh
+++ modules/oh-my-zsh/macos/install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+# TODO: This will end up conflicting with the one from antibody if it is installed that way.
 # We have to remove the `env zsh -l` at the end of the script otherwise it messes everything else up for some reason.
 [[ -d ~/.oh-my-zsh ]] || sh <(curl -fsSL 'https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh' | sed '/.*env zsh.*/d')
